f18e82b42ffb26e1d5caeff8a203694a9a3b4421
Naman Sinha <65483393+Naman794@users.noreply.github.com>
2021-03-24 22:54:40 +0530

    Update emoji.py

--- emoji.py
+++ emoji.py
@@ -7,13 +7,13 @@ import webhook
 @bot.event
 async def on_message(message):
    if ":" == message.content[0] and ":" == message.content[-1]:
-          emoji_name = message.content[1:-1]
- 
-          for emoji in message.guild.emojis:
-             if emoji_name == emoji.name:
-                 pfp = requests.get(message.author.avatar_url_as(format='png', size=256)).content
-                 hook = await message.channel.create_webhook(name=message.author.display_name, avatar=pfp)
-                 await hook.send(emoji)
-                 await message.delete()
+        emoji_name = message.content[1:-1]
+
+        for emoji in message.guild.emojis:
+            if emoji_name == emoji.name:
+                pfp = requests.get(message.author.avatar_url_as(format='png', size=256)).content
+                hook = await message.channel.create_webhook(name=message.author.display_name, avatar=pfp)
+                await hook.send(emoji)
+                await message.delete()
 
     await bot.process_commands(message)
