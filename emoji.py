0f0e2f04447f5dbdb7a37aea3bc1aca42e5622f5
Naman Sinha <65483393+Naman794@users.noreply.github.com>
2021-03-24 22:54:15 +0530

    Create emoji.py

---
+++ emoji.py
@@ -0,0 +1,19 @@
+import discord
+import webhook
+
+
+
+
+@bot.event
+async def on_message(message):
+   if ":" == message.content[0] and ":" == message.content[-1]:
+          emoji_name = message.content[1:-1]
+ 
+          for emoji in message.guild.emojis:
+             if emoji_name == emoji.name:
+                 pfp = requests.get(message.author.avatar_url_as(format='png', size=256)).content
+                 hook = await message.channel.create_webhook(name=message.author.display_name, avatar=pfp)
+                 await hook.send(emoji)
+                 await message.delete()
+
+    await bot.process_commands(message)
